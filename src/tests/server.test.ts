5c5ee9e70bbbf83073fe6d0b3627a8ee49dd2adc
ciraam <150357561+ciraam@users.noreply.github.com>
2025-11-06 16:29:39 +0100

    feat: add few test

--- src/tests/server.test.ts
+++ src/tests/server.test.ts
@@ -1,37 +1,73 @@
-import {describe, it, expect, beforeAll} from "vitest"
-import app from "../server"
+import { describe, it, expect, beforeAll } from "vitest"
+import { server } from "../main"
 import request from 'supertest'
 
 let serverTest: any
-let zipCode: string = '25000'
+const zipCode: string = '21000'
+const id: number = 1
 
 describe('TEST de Stormly', () => {
 
   beforeAll(async () => {
-    serverTest = app
+    serverTest = server
   });
+
   describe('TEST des endpoints', () => {
     it('GET /cities', async () => {
         const req = await request(serverTest)
         .get("/cities")
         .expect(200)
-        .then(res => { expect(res.body[0]).toEqual({ name: 'Besançon', zipCode: '25000' }) })
+        .then(res => { expect(res.body[0]).toEqual({ name: 'Dijon', zipCode: '21000' }) })
     })
     it('GET cities by zipCode /cities/:zipCode', async () => {
         const req = await request(serverTest)
         .get(`/cities/${zipCode}`)
         .expect(200)
-        .then(res => { expect(res.body).toEqual({ name: 'Besançon', zipCode: '25000' }) })
+        .then(res => { expect(res.body).toEqual({ name: 'Dijon', zipCode: '21000' }) })
     })
-    it('GET weather by zipCode /cities/:zipCode/weather', async () => {
+    it('GET /weather', async () => {
+        const req = await request(serverTest)
+        .get("/weather")
+        .expect(200)
+        .then(res => { expect(res.body[0]).toEqual({
+          id: 1,
+          zipCode: "21000",
+          townName: "Dijon",
+          weather: "neige"
+        }) })
+    })
+    it('GET weather report by id /weather/:id', async () => {
+        const req = await request(serverTest)
+        .get(`/weather/${id}`)
+        .expect(200)
+        .then(res => { expect(res.body).toEqual({
+          id: 1, 
+          zipCode: "21000",
+          townName: "Dijon",
+          weather: "neige"
+        }) })
+    })
+    it('GET cities weather by zipCode /cities/:zipCode/weather', async () => {
         const req = await request(serverTest)
         .get(`/cities/${zipCode}/weather`)
         .expect(200)
         .then(res => { expect(res.body).toEqual({
-          zipCode: '25000',
-	        name: 'Besançon',
+          id: 1,
+          zipCode: '21000',
+	        townName: 'Dijon',
           weather: "neige"
-        })})
+        }) })
+    })
+    it('GET weather report by id /cities/:zipCode/weather/:id', async () => {
+        const req = await request(serverTest)
+        .get(`/cities/${zipCode}/weather/${id}`)
+        .expect(200)
+        .then(res => { expect(res.body).toEqual({
+          id: 1,
+          zipCode: '21000',
+	        townName: 'Dijon',
+          weather: "neige"
+        }) })
     })
   })
 
