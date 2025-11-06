a2ac68d95595f302c42eb0e82f20b1f3967fb6fd
ciraam <150357561+ciraam@users.noreply.github.com>
2025-11-06 15:47:23 +0100

    feat: add new test

--- src/tests/server.test.ts
+++ src/tests/server.test.ts
@@ -3,6 +3,7 @@ import app from "../server"
 import request from 'supertest'
 
 let serverTest: any
+let zipCode: string = '25000'
 
 describe('TEST de Stormly', () => {
 
@@ -14,7 +15,23 @@ describe('TEST de Stormly', () => {
         const req = await request(serverTest)
         .get("/cities")
         .expect(200)
-        .then(res => { expect(res.body).toEqual('') })
+        .then(res => { expect(res.body[0]).toEqual({ name: 'Besançon', zipCode: '25000' }) })
+    })
+    it('GET cities by zipCode /cities/:zipCode', async () => {
+        const req = await request(serverTest)
+        .get(`/cities/${zipCode}`)
+        .expect(200)
+        .then(res => { expect(res.body).toEqual({ name: 'Besançon', zipCode: '25000' }) })
+    })
+    it('GET weather by zipCode /cities/:zipCode/weather', async () => {
+        const req = await request(serverTest)
+        .get(`/cities/${zipCode}/weather`)
+        .expect(200)
+        .then(res => { expect(res.body).toEqual({
+          zipCode: '25000',
+	        name: 'Besançon',
+          weather: "neige"
+        })})
     })
   })
 
