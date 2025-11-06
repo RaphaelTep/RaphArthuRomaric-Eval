53f8f3ac89f8f9c742f4c925e9f8ad2104f917d7
AnonAlly <193170968+AnonAlly@users.noreply.github.com>
2025-11-06 16:03:15 +0100

    feat: Add cities/:zipCode endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { cities } from './storage';
+import { cities, weather } from './storage';
 
 const server = express();
 
@@ -17,6 +17,20 @@ server.get('/cities', (req, res) => {
 
   res.json(cities);
 });
+
+server.get('/cities/:zipCode', (req, res) => {
+  isValidMethod(req, res, 'GET');
+
+  let findCity = cities.find(city => city.zipCode === req.params.zipCode);
+
+  if(!findCity){
+    res.status(404).json({ error: 'City not found' });
+    res.end();
+  }
+
+  res.status(200).json(findCity);
+});
+
 
 server.listen(3000, () => {
   console.log(`Server running on port 3000`);
