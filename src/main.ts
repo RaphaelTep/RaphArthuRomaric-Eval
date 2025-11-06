7397bff699ed29460120a7d0404421a48a544513
RaphaelTep <raphaeltep@gmail.com>
2025-11-06 16:10:35 +0100

    feat: Fix some type and add DELETE /cities/:zipCode endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,87 @@
-import express from 'express';
-import { cities, weather } from './storage';
+import express from "express";
+import { cities, weather } from "./storage";
 
 const server = express();
 
 server.use(express.json());
 
-function isValidMethod(req: express.Request, res: express.Response, method: string) {
-  if(req.method !== method){
-    res.status(405).json({ error: 'Method not allowed' });
-    res.end()
+function isValidMethod(
+  req: express.Request,
+  res: express.Response,
+  method: string,
+) {
+  if (req.method !== method) {
+    res.status(405).json({ error: "Method not allowed" });
+    res.end();
   }
 }
 
-server.get('/cities', (req, res) => {
-  isValidMethod(req, res, 'GET');
+server.get("/cities", (req, res) => {
+  isValidMethod(req, res, "GET");
 
   res.json(cities);
 });
 
-server.get('/cities/:zipCode', (req, res) => {
-  isValidMethod(req, res, 'GET');
+server.get("/cities/:zipCode", (req, res) => {
+  isValidMethod(req, res, "GET");
 
-  let findCity = cities.find(city => city.zipCode === req.params.zipCode);
+  let findCity = cities.find((city) => city.zipCode === req.params.zipCode);
 
-  if(!findCity){
-    res.status(404).json({ error: 'City not found' });
+  if (!findCity) {
+    res.status(404).json({ error: "City not found" });
     res.end();
   }
 
   res.status(200).json(findCity);
 });
 
-server.get('/cities/:zipCode/weather', (req, res) => {
-  isValidMethod(req, res, 'GET');
+server.get("/cities/:zipCode/weather", (req, res) => {
+  isValidMethod(req, res, "GET");
 
-  let findCity = cities.find(city => city.zipCode === req.params.zipCode);
+  let findCity = cities.find((city) => city.zipCode === req.params.zipCode);
 
-  if(!findCity){
-    res.status(404).json({ error: 'City not found' });
+  if (!findCity) {
+    res.status(404).json({ error: "City not found" });
     res.end();
   }
 
-  let findWeather = weather.find(weather => weather.zipCode === req.params.zipCode);
+  let findWeather = weather.find(
+    (weather) => weather.zipCode === req.params.zipCode,
+  );
 
-  if(!findWeather){
-    res.status(404).json({ error: 'No weather data found' });
+  if (!findWeather) {
+    res.status(404).json({ error: "No weather data found" });
     res.end();
   }
 
   res.status(200).json(findWeather);
 });
 
-server.get('/weather', (req, res) => {
+server.get("/weather", (req, res) => {
   res.status(200).json(weather);
+});
+
+server.delete("/cities/:zipCode", (req, res) => {
+  const index = cities.findIndex(
+    (t) => t.zipCode === parseInt(req.params.zipCode),
+  );
+
+  isValidMethod(req, res, "DELETE");
+
+  if (index !== -1) {
+    cities.splice(index, 1);
+
+    if (cities.length === 0) {
+      res.status(204);
+      res.end();
+    }
+
+    res.status(200).send({
+      status: "success",
+    });
+  } else {
+    res.status(404).send({ error: "Contact not found" });
+  }
 });
 
 server.listen(3000, () => {
